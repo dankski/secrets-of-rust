cc83e3691c873b24018d2c0dc351e4b84d98c235
dankski <giacomo@amoroso.me>
2025-03-23 22:37:21 +0100

    WIP add simple request

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+  let api_key = env::var("WEATHERSTACK_API_KEY").unwrap();
+  let resp = reqwest::blocking::Client::new()
+      .get("https://api.weatherstack.com/current")
+      .query(&[("query", "Zurich,CH"), ("access_key", &api_key)])
+      .send()
+      .unwrap();
+
+  println!("{}", resp.text().unwrap());
 }
