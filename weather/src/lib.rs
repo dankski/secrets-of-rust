fbd0229ec02276585b8a75894e0b6d5f02eb6644
dankski <giacomo@amoroso.me>
2025-04-02 22:41:08 +0200

    WIP Add deserialization

--- weather/src/lib.rs
+++ weather/src/lib.rs
@@ -1,6 +1,9 @@
-use anyhow::Result;
+use anyhow::{Context, Result, bail};
 
 use std::fmt::Display;
+
+use reqwest::blocking::RequestBuilder;
+use serde_json::Value;
 
 #[derive(Debug, PartialEq)]
 pub struct Weather {
@@ -10,11 +13,16 @@ pub struct Weather {
 
 impl Display for Weather {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{self:?}")
+    write!(f, "{} {:.1}C", self.summary, self.temperature)
   }
 }
 
 pub fn get_weather(location: &str, api_key: &str) -> Result<Weather> {
+  
+  if location.is_empty() {
+    bail!("Usage: weather <LOCATION>");
+  }
+
   let resp = request(location, api_key).send()?;
   let weather = deserialize(&resp.text()?)?;
   Ok(weather)
@@ -26,11 +34,33 @@ fn request(location: &str, api_key: &str) -> RequestBuilder {
     .query(&[("query", location), ("access_key", api_key)])
 }
 
+fn deserialize(json: &str) -> Result<Weather> {
+  let val: Value = serde_json::from_str(json)?;
+  
+  let temperature = val
+      .pointer("/current/temperature")
+      .and_then(Value::as_f64)
+      .with_context(|| format!("bad response: {val}"))?;
+  
+  let summary = val
+      .pointer("/current/weather_descriptions/0")
+      .and_then(Value::as_str)
+      .with_context(|| format!("bad response: {val}"))?
+      .to_string();
+  Ok(Weather {
+    temperature,
+    summary
+  })
+}
+
 #[cfg(test)]
 mod tests {
+  use std::fs;
   use std::env;
 
   use super::*;
+
+  use url::Host::Domain;
 
   #[test]
   fn get_weather_fn_returns_correct_weather_for_location() {
@@ -50,13 +80,13 @@ mod tests {
   #[test]
   fn request_builds_correct_request() {
     let req = request("Zurich,ZH", "dummy API key");
-    let req = req.build().unwra();
+    let req = req.build().unwrap();
 
-    assert_eq!(req.method(), "GET", "Wrong Method"):
+    assert_eq!(req.method(), "GET", "Wrong Method");
 
     let url = req.url();
     assert_eq!(url.host(),
-              Some(Domain("api.weatherstack.com"),
+              Some(Domain("api.weatherstack.com")),
               "Wrong host"
     );
     
@@ -66,10 +96,25 @@ mod tests {
     assert_eq!(
       params,
       vec![
-          ("query".into(), "ZUrich,ZH".into()),
+          ("query".into(), "Zurich,ZH".into()),
           ("access_key".into(), "dummy API key".into())
       ],
       "Wrong, params"
     );
-  },
+  }
+
+  #[test]
+  fn deserialize_extracts_correct_weather_from_json() {
+    let json = fs::read_to_string("tests/data/ws.json").unwrap();
+    let weather = deserialize(&json).unwrap();
+
+    assert_eq!(
+      weather,
+      Weather {
+        temperature: 11.2,
+        summary: "Sunny".into(),
+      },
+      "Wrong weather"
+    );
+  }
 }
